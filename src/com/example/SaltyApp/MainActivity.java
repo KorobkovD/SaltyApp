a5f2c8221540595de5633aeb8495dd85e1441819
Denis K <den-kor.int@mail.ru>
2015-06-05 13:46:35 +0300

    What's new: - Loading message - Scrolls on text area - Text area has fixed max value of its height, which depends on display resolution

--- src/com/example/SaltyApp/MainActivity.java
+++ src/com/example/SaltyApp/MainActivity.java
@@ -3,10 +3,16 @@ package com.example.SaltyApp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import java.util.Locale;
 
@@ -22,9 +28,25 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        // вывод тоста с сообщением о загрузке приложения
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "Приложение загружается. Подождите...", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        LinearLayout toastContainer = (LinearLayout) toast.getView();
+        ImageView waitView = new ImageView(getApplicationContext());
+        waitView.setImageResource(R.drawable.wait);
+        toastContainer.addView(waitView, 0);
+        toast.show();
+        //загрузка модуля речи
         tts = new TextToSpeech(this, this);
         btnSpeak = (Button) findViewById(R.id.button);
         txtText = (EditText) findViewById(R.id.editText);
+        // узнать размеры экрана
+        Display display = getWindowManager().getDefaultDisplay();
+        DisplayMetrics metricsB = new DisplayMetrics();
+        display.getMetrics(metricsB);
+        // установка максимального значения высоты поля ввода текста
+        txtText.setMaxHeight(metricsB.heightPixels-300);
         btnSpeak.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
